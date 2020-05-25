6fcf16ec5bb4274a72274167bcc174d531d97ac0
Dave Cook <DaveCook@hotmail.co.uk>
2020-05-25 17:38:42 +0700

    about to add scriptTags

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import { EmptyState, Layout, Page } from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
-import store from 'store-js';
+import ProductList from "../components/ProductList";
+import store from "store-js";
 
 function Index() {
-
   const [modal, setModal] = useState({ open: false });
-  const emptyState = !store.get('ids');
+  const emptyState = !store.get("ids");
 
-  function handleSelection(resources){
-    const idsFromResourcePicker = resources.selection.map(product => product.id);
+  function handleSelection(resources) {
+    const idsFromResourcePicker = resources.selection.map(
+      (product) => product.id
+    );
     setModal({ open: false });
-    store.set('ids',idsFromResourcePicker);
+    store.set("ids", idsFromResourcePicker);
   }
 
   return (
@@ -21,24 +23,29 @@ function Index() {
         showVariants={false}
         open={modal.open}
         onCancel={() => setModal({ open: false })}
-        onSelection={resources => handleSelection(resources)}
+        onSelection={(resources) => handleSelection(resources)}
       />
-      <Layout>
-        <EmptyState
-          heading="Manage your inventory transfers"
-          action={{ 
-            content: "Select products",
-            onAction: () => setModal({ open: true }),
-          }}
-          secondaryAction={{
-            content: "Learn more",
-            url: "https://help.shopify.com",
-          }}
-          image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-        >
-          <p>Select products</p>
-        </EmptyState>
-      </Layout>
+
+      {emptyState ? (
+        <Layout>
+          <EmptyState
+            heading="Manage your inventory transfers"
+            action={{
+              content: "Select products",
+              onAction: () => setModal({ open: true }),
+            }}
+            secondaryAction={{
+              content: "Learn more",
+              url: "https://help.shopify.com",
+            }}
+            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+          >
+            <p>Select products</p>
+          </EmptyState>
+        </Layout>
+      ) : (
+        <ProductList></ProductList>
+      )}
     </Page>
   );
 }
